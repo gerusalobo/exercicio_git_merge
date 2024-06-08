c158c8526bdebead7ff3fa225e121fe5aa72a68b
gerusalobo <gerusalobo@gmail.com>
2024-06-08 17:23:11 -0300

    desafio

---
+++ app_desafio1.js
@@ -0,0 +1,20 @@
+alert('Boas vindas ao nosso site!');
+var nome = 'Lua';
+console.log(nome);
+var idade = 25;
+console.log(idade);
+var numeroDeVendas = 50;
+console.log(numeroDeVendas);
+var saldoDisponivel = 1000;
+console.log(saldoDisponivel);
+alert('Erro! Preencha todos os campos');
+var mensagemDeErro = 'Erro! Preencha todos os campos seu burro';
+alert(mensagemDeErro);
+nome = prompt('Nome do usuario');
+console.log(nome);
+idade = prompt('Idade do usuario');
+console.log(idade);
+if (idade >= 18) {
+    alert('Pode tirar a habilitação');
+}
+else {console.log('Muito novo');}
